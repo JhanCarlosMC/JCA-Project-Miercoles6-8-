8188a13850e0d99bf511bb9a1f76504d55b2f7c1
JhanCarlosMC <jcmartinez@uniquindio.edu.co>
2025-08-13 18:56:49 -0500

    test: Creacion de proyecto ejemplo basicos js

---
+++ ejemplos/ejemploJS/script.js
@@ -0,0 +1,61 @@
+//Conceptos Inciales JS
+// ---------- Variables ---------
+// Declaracion -> Existencia/Creacion del elemento
+// Asignacion -> Darle valor a un elemento ya creado
+// Inicializacion -> Declaracion y Asignacion en la misma linea
+//TipoVariable NombreVariable = ValorVariable;
+let x; //Declaracion
+x = 10; // Asignacion
+const pi = 3.1416; // Inicializacion
+
+// ------------ Tipos de Datos ----------------
+// Numericos -> Number (10 1.0 -10 -10.0)
+let num = 10;
+
+// Cadenas de Texto -> String ("Hola Mundo" 'Hola Mundo')
+let cadena = "Hola";
+
+// Logicos -> Boolean (True False)
+let logico = true;
+let logico2 = false;
+
+// Objetos -> Object (Un elemento que cuenta con caracteristicas y comportamiento)
+//              persona = {nombre: "Jhan", edad: 24}
+let computador = {marca: "hp", cpu: "i7", ram:"16GB"}
+
+// Arreglos -> Array (Un conjunto de elementos)
+//              edades = [20,24,18,19]
+let edades = [20,24,18,19];
+
+// Nulo -> Null (Elemento vacio)
+let nulo = null;
+
+// Indefinido -> Undefined (Variable que no ha sido asignada)
+let indefinido;
+
+let id = "12121";
+id = "hola";
+const id2 = "121211";
+id2 = "Adios";
+
+// ---------------- Operadores ----------------
+//Aritmeticos
+let suma = 2+2;
+let resta = 2-1;
+let multiplicacion = 2*2;
+let division = 2/2;
+let modulo = 2%2;
+
+//Relaciones
+let igual = 2=="2";
+let igualdadEstricta = 2==="2";
+let diferente = 2!=2;
+let mayor = 2>2;
+let mayorIgual = 2>=2;
+let menor = 2<2;
+let menorIgual = 2<=2;
+
+//Logicos
+let and = true && true;
+let or = true || false;
+let not = !true;
