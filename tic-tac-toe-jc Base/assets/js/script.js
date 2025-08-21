8d36a782010e60413fb352b9a090dff72c551ff6
JhanCarlosMC <jcmartinez@uniquindio.edu.co>
2025-08-20 19:49:52 -0500

    Feat: Finalizacion del proyecto Triki, con funciones js finales

--- tic-tac-toe-jc Base/assets/js/script.js
+++ tic-tac-toe-jc Base/assets/js/script.js
@@ -5,7 +5,7 @@ class TicTacToe{
         this.board = Array(9).fill("");
         this.currentPlayer = "X";
         this.gameActive = true;
-        this.score = {
+        this.scores = {
             X: 0,
             O: 0
         };
@@ -30,6 +30,16 @@ class TicTacToe{
 
         this.createAddEventListeners();
         this.updateDisplayGame();
+
+        // let titulo = document.getElementById("title");
+        // titulo.textContent = "Triki";
+        // titulo.classList.add("title");
+
+        // titulo.addEventListener("click", () => {
+        //     titulo.style.color = "red";
+        // });
+
+
     }
 
     createAddEventListeners(){
@@ -56,6 +66,93 @@ class TicTacToe{
         this.currentPlayerDisplay.textContent = this.currentPlayer;
         this.currentPlayerDisplay.style.color = this.currentPlayer === "X" ? 'red' : 'blue';
     }
+ 
+    makeMove(index, cell){
+        this.board[index] = this.currentPlayer;
+        cell.textContent = this.currentPlayer;
+        cell.classList.add(this.currentPlayer.toLowerCase());
+
+        if(this.checkWinner()){ // Hay ganador
+            this.handleGameEnd("win");
+        }else if(this.checkDraw()){ // Empate
+            this.handleGameEnd("draw");
+        }else{ // Siguiente turno
+            this.switchPlayer();
+        }
+    }
+
+    checkWinner(){
+        for(let condition of this.winningConditions){
+            const [a, b, c] = condition;
+            if(this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]){
+                this.highlightWinningCells(condition);
+                return true; // Hay ganador
+            }
+        }
+        return false; // No hay ganador
+    }
+
+    checkDraw(){
+        return this.board.every(cell => cell !== "");
+    }
+
+    handleGameEnd(result){
+        this.gameActive = false;
+
+        if(result === "win"){ //Ganador
+            this.scores[this.currentPlayer]++;
+            this.gameStatus.textContent = `¡${this.currentPlayer} ha ganado!`;
+            this.gameStatus.className = "game-status winner";
+        }else{ //Empate
+            this.gameStatus.textContent = `¡Empate!`;
+            this.gameStatus.className = "game-status draw";
+        }
+
+        this.updateScoreDisplay();
+    }
+
+    highlightWinningCells(conditionWinning){
+        conditionWinning.forEach(index => {
+            this.cells[index].classList.add("winning");
+        })
+    }
+
+    switchPlayer(){
+        this.currentPlayer = (this.currentPlayer === "X") ? "O" : "X"; //Ternario
+        this.updateDisplayGame();
+    }
+
+    updateScoreDisplay(){
+        this.scoreX.textContent = this.scores.X;
+        this.scoreO.textContent = this.scores.O;
+    }
+
+    resetGame(){
+        this.board.fill("");
+        this.gameActive = true;
+        this.currentPlayer = "X";
+
+        this.cells.forEach(cell => {
+            cell.textContent = "";
+            cell.classList.remove("x", "o", "winning");
+        })
+
+            this.gameStatus.textContent = ``;
+            this.gameStatus.className = "game-status";
+
+            this.updateDisplayGame();
+    }
+
+    newGame(){
+        this.resetGame();
+        this.scores = {
+            X: 0,
+            O: 0
+        };
+
+        this.updateScoreDisplay();
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", function(){
